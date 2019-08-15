a49a576ec06d0be879d4464228b9d470af7cdac0
prgrmr126 <45293734+prgrmr126@users.noreply.github.com>
2019-08-15 07:44:43 -0400

    Revisions and restructuring

--- solve.py
+++ solve.py
@@ -1,21 +1,211 @@
 from copy import deepcopy
 import time
 
-class Node:
+def listIncludes(line, searchInteger, giveCount=False):
+	if (str(searchInteger) in [str(x) for x in line]):
+		if (giveCount):
+			return(True, [str(x) for x in line].count(str(searchInteger)))
+		else:
+			return(True)
+	else:
+		if (giveCount):
+			return(False, 0)
+		else:
+			return(False)
 
-	def __init__(self, x, y, value):
-		self.x = x
-		self.y = y
-		self.value = value
+def getAdjacentBoxes(num, dir):
+	if (dir == 'horizontal'):
+		boxes = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+	elif (dir == 'vertical'):
+		boxes = [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
+	for row in boxes:
+		if num in row:
+			row.remove(num)
+			return(row)
 
-	def getColumn(self, board):
+def checkSingle(possibles, dir):
+	if (len(possibles) > 0):
+		if (dir == 'horizontal'): val = possibles[0].i
+		elif (dir == 'vertical'): val = possibles[0].j
+		for possible in possibles:
+			if (dir == 'horizontal' and possible.i != val):
+				return(False)
+			elif (dir == 'vertical' and possible.j != val):
+				return(False)
+		return(True)
+	else:
+		return(False)
+
+class Board:
+	def __init__(self, dataSet):
+		self.board = dataSet
+
+	def getBox(self, boxNum):
+		boxNum *= 3
+		box = []
+		x = (boxNum // 9) * 3
+
+		for i in range(3):
+			box.append(self.board[x+i][boxNum % 9:boxNum % 9 + 3])
+		return(box)
+
+	def getColumn(self, columnNumber):
 		column = []
 		for i in range(9):
-			column.append(board[i][self.x])
+			column.append(self.board[i][columnNumber])
 		return(column)
 
-	def getRow(self, board):
-		return(board[self.y])
+	def getRow(self, rowNumber):
+		return(self.board[rowNumber])
+
+	def checkFull(self):
+		for i in range(9):
+			if (listIncludes(self.board[i], 0) > 0): return(False)
+		return(True)
+
+	def checkBoard(self):
+		for i in range(9):
+			if (listIncludes(self.board[i], 0, True)[1] > 0):
+				return(False)
+
+			for j in range(1, 10):
+				if (listIncludes(self.board[i], j, True)[1] > 1):
+					return(False)
+
+			for j in range(1, 10):
+				if (listIncludes(self.getColumn(i), j, True)[1] > 1):
+					return(False)
+
+		return(True)
+
+	def fill(self):
+		code = 1
+
+		for i in range(1, 10):
+			for j in range(9):
+				impossible = False
+				possibles = []
+				box = self.getBox(j)
+				for a in range(3):
+					for b in range(3):
+						if (box[a][b].value == 0):
+							if ((not listIncludes(self.getColumn(box[a][b].j), i)) and (not listIncludes(self.getRow(box[a][b].i), i))):
+								possibles.append(box[a][b])
+						elif (box[a][b].value == i): impossible = True
+
+				if (not impossible):
+					if ((len(possibles) > 1)):
+						pass
+					elif ((len(possibles) == 0)):
+						code = 2
+						return(code)
+					else:
+						possibles[0].setValue(i)
+						code = 0
+		return(code)
+
+	def completeLines(self):
+		code = 1
+
+		for i in range(9):
+			absentColumn = []
+			columnIndex = 0
+
+			absentRow = []
+			rowIndex = 0
+			for j in range(1, 10):
+				if j not in [str(x) for x in self.getColumn(i)]:
+					absentColumn.append(j)
+
+				if (self.getColumn(i)[j - 1].value == 0):
+					columnIndex = j - 1
+
+				if j not in [str(x) for x in self.getRow(i)]:
+					absentRow.append(j)
+
+				if (self.getRow(i)[j - 1].value == 0):
+					rowIndex = j - 1
+
+			if (len(absentColumn) == 1):
+				self.getRow(i)[columnIndex].setValue(absentColumn[0])
+
+			if (len(absentRow) == 1):
+				self.getRow(i)[rowIndex].setValue(absentRow[0])
+
+		return(code)
+
+	def superFill(self):
+
+		code = 1
+
+		for i in range(1, 10):
+			allPossibles = []
+
+			for j in range(9):
+				impossible = False
+				possibles = []
+				box = self.getBox(j)
+				for a in range(3):
+					for b in range(3):
+						if (box[a][b].value == 0):
+							if (not listIncludes(self.getColumn(box[a][b].j), i)):
+								if (not listIncludes(self.getRow(box[a][b].i), i)):
+									possibles.append(box[a][b])
+						elif (box[a][b].value == i):
+							impossible = True
+				if impossible:
+					possibles = []
+
+				allPossibles.append(possibles)
+
+			for j in range(9):
+				selfBox = allPossibles[j]
+
+				rows = []
+				for num in getAdjacentBoxes(j, 'horizontal'):
+					rows.append(allPossibles[num])
+
+				cols = []
+				for num in getAdjacentBoxes(j, 'vertical'):
+					cols.append(allPossibles[num])
+
+				for rowBox in rows:
+					if checkSingle(rowBox, 'horizontal'):
+						newPossibles = []
+						for selfPossible in selfBox:
+							if selfPossible.i != rowBox[0].i:
+								newPossibles.append(selfPossible)
+						selfBox = newPossibles
+
+				for colBox in cols:
+					if checkSingle(colBox, 'vertical'):
+						newPossibles = []
+						for selfPossible in selfBox:
+							if selfPossible.j != colBox[0].j:
+								newPossibles.append(selfPossible)
+						selfBox = newPossibles
+
+				if (len(selfBox) == 1):
+					selfBox[0].setValue(i)
+					code = 0
+		return(code)
+
+	def print(self):
+		print('\n')
+
+		for i in range(9):
+			printline = '  '.join([' '.join([str(x) for x in self.board[i][0:3]]), ' '.join([str(x) for x in self.board[i][3:6]]), ' '.join([str(x) for x in self.board[i][6:9]])])
+			printline = printline.replace('0', ' ')
+			print(printline)
+
+			if ((i + 1) % 3 == 0):
+				print('')
+
+class Node:
+	def __init__(self, i, j, value):
+		self.i = i
+		self.j = j
+		self.value = value
 
 	def setValue(self, newValue):
 		self.value = newValue
@@ -26,254 +216,12 @@ class Node:
 	def __str__(self):
 		return(str(self.value))
 
-##region BoardFunctions
-def printBoard(chart):
-	print('\n')
-
-	board = deepcopy(chart)
-
-	for i in range(9):
-		for j in range(9):
-			board[i][j] = str(board[i][j])
-	for i in range(9):
-		printline = '  '.join([' '.join(board[i][0:3]), ' '.join(board[i][3:6]), ' '.join(board[i][6:9])])
-		printline = printline.replace('0', ' ')
-		print(printline)
-
-		if ((i + 1) % 3 == 0):
-			print('')
-
-def getBox(board, num):
-	num *= 3
-	box = []
-	x = (num // 9) * 3
-
-	for i in range(3):
-		box.append(board[x+i][num % 9:num % 9 + 3])
-	return(box)
-
-def listIncludes(line, num):
-	for i in line:
-		if (i.value == num):
-			return(True)
-	return(False)
-
-def countNode(container, num):
-	counter = 0
-	for item in container:
-		if (item.value == num):
-			counter += 1
-
-	return(counter)
-
-def boardFull(board):
-	for i in range(9):
-		for item in board[i]:
-			if (item.value == 0):
-				return(False)
-	return(True)
-
-def checkErrors(board):
-	for i in range(9):
-		for j in range(1, 10):
-			if (countNode(board[i], j) > 1):
-				return(True)
-
-		for j in range(1, 10):
-			if (countNode(board[0][i].getColumn(board), j) > 1):
-				return(True)
-
-	return(False)
-
-def checkBoard(board):
-	errors = checkErrors(board)
-	if (not errors):
-		for i in range(9):
-			for j in range(9):
-				if (board[i][j].value == 0):
-					return(False)
-		return(True)
-	else:
-		return(False)
-
-def countZero(board):
-	totalCount = 0
-
-	for i in range(9):
-		totalCount += countNode(board[i], 0)
-
-	return(totalCount)
-
-##endregion
-
-##region SolveTechniques
-def fill(board):
-	code = 1
-
-	for i in range(1, 10):
-		for j in range(9):
-			impossible = False
-			possibles = []
-			box = getBox(board, j)
-			for a in range(3):
-				for b in range(3):
-					if (box[a][b].value == 0):
-						if (not listIncludes(box[a][b].getColumn(board), i)):
-							if (not listIncludes(box[a][b].getRow(board), i)):
-								possibles.append(box[a][b])
-					elif (box[a][b].value == i):
-						impossible = True
-
-			if (not impossible):
-				if ((len(possibles) > 1)):
-					pass
-				elif ((len(possibles) == 0)):
-					code = 2
-				else:
-					possibles[0].setValue(i)
-					code = 0
-	return (board, code)
-
-def lineComplete(board):
-	code = 1
-
-	for i in range(9):
-		zeroCount = 0
-		for item in board[i]:
-			if (item.value == 0):
-				zeroCount += 1
-
-		if (zeroCount == 1):
-			possibles = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-			for j in range(len(board[i])):
-				if (board[i][j].value != 0):
-					possibles.remove(board[i][j].value)
-				else:
-					zeroSet = j
-
-			board[i][zeroSet].setValue(possibles[0])
-			NO_CHANGE = False
-
-	for i in range(9):
-		column = board[0][i].getColumn(board)
-		zeroCount = 0
-		for item in column:
-			if (item.value == 0):
-				zeroCount += 1
-
-		if (zeroCount == 1):
-			possibles = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-			for j in range(len(column)):
-				if (column[j].value != 0):
-					possibles.remove(column[j].value)
-				else:
-					zeroSet = j
-
-			board[zeroSet][i].setValue(possibles[0])
-			code = 0
-
-	return (board, code)
-
-##endregion
-
-##region SuperFill
-def superFill(board):
-
-	NO_CHANGE = True
-	for i in range(1, 10):
-		allPossibles = []
-
-		for j in range(9):
-			impossible = False
-			possibles = []
-			box = getBox(board, j)
-			for a in range(3):
-				for b in range(3):
-					if (box[a][b].value == 0):
-						if (not listIncludes(box[a][b].getColumn(board), i)):
-							if (not listIncludes(box[a][b].getRow(board), i)):
-								possibles.append(box[a][b])
-					elif (box[a][b].value == i):
-						impossible = True
-			if impossible:
-				possibles = []
-
-			allPossibles.append(possibles)
-
-		for j in range(9):
-			selfBox = allPossibles[j]
-
-			rows = []
-			for num in getRowBoxes(j):
-				rows.append(allPossibles[num])
-
-			cols = []
-			for num in getColBoxes(j):
-				cols.append(allPossibles[num])
-
-			for rowBox in rows:
-				if checkSingleRow(rowBox):
-					newPossibles = []
-					for selfPossible in selfBox:
-						if selfPossible.y != rowBox[0].y:
-							newPossibles.append(selfPossible)
-					selfBox = newPossibles
-
-			for colBox in cols:
-				if checkSingleCol(colBox):
-					newPossibles = []
-					for selfPossible in selfBox:
-						if selfPossible.x != colBox[0].x:
-							newPossibles.append(selfPossible)
-					selfBox = newPossibles
-
-			if (len(selfBox) == 1):
-				selfBox[0].setValue(i)
-				NO_CHANGE = False
-	return (board, NO_CHANGE)
-
-def getRowBoxes(num):
-	boxes = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-	for row in boxes:
-		if num in row:
-			row.remove(num)
-			return(row)
-
-def getColBoxes(num):
-	boxes = [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
-	for col in boxes:
-		if num in col:
-			col.remove(num)
-			return(col)
-
-def checkSingleRow(possibles):
-	if (len(possibles) > 0):
-		yVal = possibles[0].y
-		for possible in possibles:
-			if (possible.y != yVal):
-				return(False)
-		return(True)
-	else:
-		return(False)
-
-def checkSingleCol(possibles):
-	if (len(possibles) > 0):
-		xVal = possibles[0].x
-		for possible in possibles:
-			if (possible.x != xVal):
-				return(False)
-		return(True)
-	else:
-		return(False)
-##endregion
-
-##region RecursiveGuessing
-def findChoice(board):
+def findChoice(grid):
 	emptySpots = []
 	for i in range(9):
 		counter = 0
 		index = i
-		box = getBox(board, i)
+		box = grid.getBox(i)
 		for a in range(3):
 			for b in range(3):
 				if (box[a][b].value == 0):
@@ -284,35 +232,36 @@ def findChoice(board):
 
 	i = emptySpots.index(min(emptySpots))
 
-	box = getBox(board, i)
+	box = grid.getBox(i)
 	possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 	counter = 0
 	for a in range(3):
 		for b in range(3):
 			if (box[a][b].value == 0):
-				zeroNode = (box[a][b].y, box[a][b].x)
+				zeroNode = (box[a][b].i, box[a][b].j)
 			else:
 				possibilities.remove(box[a][b].value)
 
 	return (zeroNode, possibilities)
 
 def solve(board):
-	while (not boardFull(board)):
-		board, res = fill(board)
+	while (not board.checkFull()):
+		res = board.fill()
 		if (res == 1):
-			board, res = lineComplete(board)
+			res = board.completeLines()
 			if (res == 1):
-				board, res = superFill(board)
+				res = board.superFill()
 				if (res == 1):
 					saveBoard = deepcopy(board)
 					coord, opts = findChoice(saveBoard)
 
 					isSolved = False
+
 					for i in range(len(opts)):
 						if (not isSolved):
-							if (not listIncludes(saveBoard[coord[0]][coord[1]].getColumn(saveBoard), opts[i])):
-								if (not listIncludes(saveBoard[coord[0]][coord[1]].getRow(saveBoard), opts[i])):
-									saveBoard[coord[0]][coord[1]].setValue(opts[i])
+							if (not listIncludes(saveBoard.getColumn(coord[1]), opts[i])):
+								if (not listIncludes(saveBoard.getRow(coord[0]), opts[i])):
+									saveBoard.board[coord[0]][coord[1]].setValue(opts[i])
 
 									res = solve(deepcopy(saveBoard))
 
@@ -320,7 +269,7 @@ def solve(board):
 										if (i + 1 == len(opts)):
 											return(1)
 										else:
-											saveBoard[coord[0]][coord[1]].setValue(0)
+											saveBoard = deepcopy(board)
 									else:
 										board = deepcopy(res)
 										isSolved = True
@@ -337,7 +286,6 @@ def solve(board):
 			return(1)
 
 	return(board)
-##endregion
 
 def main():
 	boardToRun = input('Which board would you like to solve? (Must be corresponding to file name): ')
@@ -346,21 +294,26 @@ def main():
 
 	with open('./boards/'+boardToRun+'.txt', 'r') as f:
 		content = f.read()
-		board = [[int(x) for x in list(row)] for row in content.split('\n')[:-1]]
+		grid = [[int(x) for x in list(row)] for row in content.split('\n')[:-1]]
 
 	for i in range(9):
 		for j in range(9):
-			board[i][j] = Node(j, i, board[i][j])
+			grid[i][j] = Node(i, j, grid[i][j])
 
-	printBoard(board)
+	board = Board(grid)
+
+	board.print()
 
 	board = solve(board)
 
-	printBoard(board)
+	if (board == 2):
+		print('IMPOSSIBLE')
+	else:
+		board.print()
 
-	print('BOARD SOLVED:', checkBoard(board))
+		print('BOARD SOLVED:', board.checkBoard())
 
-	print('\n--- %s seconds ---' % (time.time() - start_time))
+		print('\n--- %s seconds ---' % (time.time() - start_time))
 
 if __name__ == '__main__':
 	main()
